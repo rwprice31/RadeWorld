3d0d8621d0e5c1adee9f95a5d2a874e0fbca392e
smccrav1 <smccrav1@ggc.edu>
2014-11-24 21:04:35 -0500

    Project updated

---
+++ src/main/Items.java
@@ -0,0 +1,21 @@
+package main;
+// class for items
+public class Items {
+
+	private String name;
+	private String roomNumber;
+
+	public Items(String _name, String _roomNumber) {
+		name = _name;
+		roomNumber = _roomNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getroomNumber() {
+		return roomNumber;
+	}
+
+}
